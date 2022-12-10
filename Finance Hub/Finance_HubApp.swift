b8dddfd349dd3c3f3753369aeaf325b2a5d708f8
brunnohofmann <brunnohofmann@hotmail.com>
2022-12-10 18:16:59 +0100

    Initial Commit

---
+++ Finance Hub/Finance_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_HubApp.swift
+//  Finance Hub
+//
+//  Created by Brunno Hofmann on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct Finance_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finance HubUITests/Finance_HubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Finance_HubUITestsLaunchTests.swift
+//  Finance HubUITests
+//
+//  Created by Brunno Hofmann on 10.12.22.
+//
+
+import XCTest
+
+class Finance_HubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
