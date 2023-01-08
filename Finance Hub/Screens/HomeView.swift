eaad3cc9f73f8be7e168e80bae0bab278e7db307
brunnohofmann <brunnohofmann@hotmail.com>
2023-01-08 18:54:28 +0100

    Display Outflows data on home screen

--- Finance Hub/Screens/HomeView.swift
+++ Finance Hub/Screens/HomeView.swift
@@ -1,15 +1,45 @@
-//
-//  HomeView.swift
-//  Finance Hub
-//
-//  Created by Brunno Hofmann on 10.12.22.
-//
-
 import SwiftUI
 
 struct HomeView: View {
+    @State private var isPresentingEditView = false
+    @State private var outflows: [Outflow] = []
+    
+    func loadOutflows(){
+        let jsonData = readLocalJsonFile(forName: "Outflows")
+        if let data = jsonData {
+            if let sampleRecordObj = parse(jsonData: data){
+                outflows = sampleRecordObj
+                print("outflows \(sampleRecordObj)")
+            }
+        }
+    }
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(outflows, id: \.category) { outflow in
+                Section(header: Text(outflow.category).font(.headline)){
+                    ForEach(outflow.summary!, id: \.amount){ summary in
+                        HStack{
+                            Text(summary.subcategory)
+                            Spacer()
+                            Text(String(summary.amount))
+                        }
+                    }
+                }
+            }
+            
+        }
+        .navigationBarTitle("Finance Hub")
+        .toolbar {
+            Button(action: {
+                print("button pressed")
+            }){
+                Label("", systemImage: "magnifyingglass")
+            }
+            
+        }
+        .onAppear(perform: loadOutflows)
     }
 }
 
